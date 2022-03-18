9d45db61c525cf90d33e78372ea43a0175c7541b
Rathish Vijendra <Vijendra.Rathish@syscolabs.com>
2022-03-18 14:29:19 +0530

    CRUD_operations_cart_and_order

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,5 +1,74 @@
 const router = require("express").Router()
+const Cart = require("../models/Cart")
+const {
+    verifyToken,
+    verifyAuthorizeToken,
+    verifyAdminToken
+} = require("./tokenVerification")
 
 
+//Create Cart
+router.post("/", verifyToken, async (req, res) => {
+    const newCart = new Cart(req.body)
+
+    try {
+        const addedCart = await newCart.save()
+        res.status(200).json(addedCart)
+    } catch (error) {
+        res.status(501).json(error)
+    }
+})
+
+//Update Cart
+router.put("/:id", verifyAuthorizeToken, async (req, res) => {
+
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {
+            new: true
+        })
+
+        res.status(200).json(updatedCart)
+    } catch (error) {
+        res.status(501).json(error)
+    }
+})
+
+//Delete Cart
+router.delete("/:id", verifyAuthorizeToken, async (req, res) => {
+    try {
+
+        await Cart.findByIdAndDelete(req.params.id)
+        res.status(200).json("Successfully Deleted Cart")
+
+    } catch (error) {
+        res.status(501).json(error)
+    }
+})
+
+//Get Cart of User
+router.get("/find/:userID", verifyAuthorizeToken, async (req, res) => {
+    try {
+
+        const cart = await Cart.findOne({
+            userID: req.params.userID
+        })
+        res.status(200).json(cart)
+
+    } catch (error) {
+        res.status(501).json(error)
+    }
+})
+
+//Get All 
+router.get("/", verifyAdminToken, async (req, res) => {
+    try {
+        const carts = await Cart.find()
+        res.status(200).json(carts)
+    } catch (error) {
+        res.status(501).json(error)
+    }
+})
 
 module.exports = router
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,5 +1,117 @@
 const router = require("express").Router()
+const Order = require("../models/Order")
+const {
+    verifyToken,
+    verifyAuthorizeToken,
+    verifyAdminToken
+} = require("./tokenVerification")
 
 
+//Create Order
+router.post("/", verifyToken, async (req, res) => {
+    const newOrder = new Order(req.body)
+
+    try {
+        const addedOrder = await newOrder.save()
+        res.status(200).json(addedOrder)
+    } catch (error) {
+        res.status(501).json(error)
+    }
+})
+
+//Update Order
+router.put("/:id", verifyAdminToken, async (req, res) => {
+
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {
+            new: true
+        })
+
+        res.status(200).json(updatedOrder)
+    } catch (error) {
+        res.status(501).json(error)
+    }
+
+})
+
+//Delete Order
+router.delete("/:id", verifyAdminToken, async (req, res) => {
+    try {
+
+        await Order.findByIdAndDelete(req.params.id)
+        res.status(200).json("Successfully Deleted Order")
+
+    } catch (error) {
+        res.status(501).json(error)
+    }
+})
+
+//Get Orders of User
+router.get("/find/:userID", verifyAuthorizeToken, async (req, res) => {
+    try {
+
+        const orders = await Order.find({
+            userID: req.params.userID
+        })
+        res.status(200).json(orders)
+
+    } catch (error) {
+        res.status(501).json(error)
+    }
+})
+
+//Get All Orders
+router.get("/", verifyAdminToken, async (req, res) => {
+    try {
+        const orders = await Order.find()
+        res.status(200).json(orders)
+    } catch (error) {
+        res.status(501).json(error)
+    }
+})
+
+//Get Stats Of Order
+router.get("/statsOrder", verifyAdminToken, async (req, res) => {
+
+    const date = new Date()
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1))
+    const prevMonth = new Date(date.getMonth(lastMonth.getMonth() - 1))
+    
+
+    try {
+        const income = await Order.aggregate([{
+                $match: {
+                    createdAt: {
+                        $gte: prevMonth
+                    }
+                }
+            },
+            {
+                $project: {
+                    month: {
+                        $month: "$createdAt"
+                    },
+                    sales: "$amount",
+                },
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: {
+                        $sum: "$sales"
+                    }
+                }
+            }
+        ])
+        
+        res.status(200).json(income)
+
+    } catch (error) {
+        res.status(501).json(error)
+    }
+
+})
 
 module.exports = router
